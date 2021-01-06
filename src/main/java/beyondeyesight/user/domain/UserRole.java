7fcc68a86e6ef04d8775d3505469c0b4e9a63571
junk-sound <wom2277@naver.com>
2021-01-06 22:41:55 +0900

    feat: UserRole

---
+++ src/main/java/beyondeyesight/user/domain/UserRole.java
@@ -0,0 +1,66 @@
+package beyondeyesight.user.domain;
+
+import java.util.Objects;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@Table("user_role")
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class UserRole extends BaseEntity {
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "role_id", nullable = false)
+    private Role role;
+
+    public UserRole(User user, Role role) {
+        // todo: check! nullable로 이미 db 레이어에서 검증이 있기에 중복...? 없는 게 나을지 확인
+        validate(user, role);
+        this.user = user;
+        this.role = role;
+    }
+
+    private void validate(User user, Role role) {
+        if (Objects.isNull(user) || Objects.isNull(role)) {
+            throw new IllegalArgumentException(String.format("%s 혹은 %s가 null 입니다.", user, role));
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "UserRole{" +
+            "user=" + user +
+            ", role=" + role +
+            '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        if (!super.equals(o)) {
+            return false;
+        }
+        UserRole userRole = (UserRole) o;
+        return Objects.equals(user, userRole.user) &&
+            Objects.equals(role, userRole.role);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), user, role);
+    }
+}
