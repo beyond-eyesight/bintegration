b650facecd113d8cd7a9fb9dbde28ef8114f57cb
Geunwon Lim <wom2277@naver.com>
2021-02-03 17:01:08 +0900

    deploy: access (#46)

---
+++ access/run.sh
@@ -0,0 +1,4 @@
+echo "********************************************************"
+echo ">>>>>>>>>>>> Starting access-service "
+echo "********************************************************"
+java -jar /usr/local/access-service/access-0.0.1.jar
