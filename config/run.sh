dffff2175db9059c33da0925b697ed20ff76f211
junk-sound <wom2277@naver.com>
2021-02-03 17:18:40 +0900

    deploy: config

---
+++ config/run.sh
@@ -0,0 +1,4 @@
+echo "********************************************************"
+echo ">>>>>>>>>>>> Starting config-service "
+echo "********************************************************"
+java -jar /usr/local/config-service/config-0.0.1.jar
