c240b19ffcd27a793f77796fdff9ad038ad58fbc
Geunwon Lim <wom2277@naver.com>
2021-02-03 17:36:32 +0900

    deploy: discovery (#49)

---
+++ discovery/run.sh
@@ -0,0 +1,4 @@
+echo "********************************************************"
+echo ">>>>>>>>>>>> Starting discovery-service "
+echo "********************************************************"
+java -jar /usr/local/discovery-service/discovery-0.0.1.jar
