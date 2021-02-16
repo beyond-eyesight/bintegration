b7e774a9af01a2cd42931c84feeb8330a42a8e42
junk-sound <wom2277@naver.com>
2021-02-16 12:04:38 +0900

    connect to aws es

---
+++ area/src/main/java/beyondeyesight/area/config/AWSStaticCredentialsConfiguration.java
@@ -0,0 +1,24 @@
+package beyondeyesight.area.config;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AWSStaticCredentialsConfiguration {
+    @Value("${aws.es.accessKey}")
+    private String esAccessKey = null;
+
+    @Value("${aws.es.secretKey}")
+    private String esSecretKey = null;
+
+    // todo 하드코딩 스타일 지우고, AWSCredentialsConfiguration로 수정.
+    @Bean
+    public AWSStaticCredentialsProvider awsDynamoCredentialsProviderDevelopment() {
+        return new AWSStaticCredentialsProvider(new BasicAWSCredentials(
+            esAccessKey, esSecretKey));
+    }
+
+}
--- area/src/main/java/beyondeyesight/area/config/RestClientConfig.java
+++ area/src/main/java/beyondeyesight/area/config/RestClientConfig.java
@@ -1,16 +1,21 @@
 package beyondeyesight.area.config;
 
+import com.amazonaws.auth.AWS4Signer;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import lombok.RequiredArgsConstructor;
+import org.apache.http.HttpHost;
+import org.apache.http.HttpRequestInterceptor;
+import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.elasticsearch.client.ClientConfiguration;
-import org.springframework.data.elasticsearch.client.RestClients;
 import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfiguration;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 import org.springframework.lang.NonNull;
 
 @Configuration
 @EnableElasticsearchRepositories(basePackages = "beyondeyesight.area.infra.persistence")
+@RequiredArgsConstructor
 public class RestClientConfig extends AbstractElasticsearchConfiguration {
 
     @Value("${elasticsearch.host}")
@@ -19,15 +24,17 @@ public class RestClientConfig extends AbstractElasticsearchConfiguration {
     @Value("${elasticsearch.port}")
     private String port;
 
+    private final AWSStaticCredentialsProvider awsStaticCredentialsProvider;
 
     @Override
     @NonNull
     public RestHighLevelClient elasticsearchClient() {
 
-        ClientConfiguration clientConfiguration = ClientConfiguration.builder()
-            .connectedTo(String.format("%s:%s", host, port))
-            .build();
-
-        return RestClients.create(clientConfiguration).rest();
+        AWS4Signer signer = new AWS4Signer();
+        String serviceName = "es";
+        signer.setServiceName(serviceName);
+        signer.setRegionName("ap-northeast-2");
+        HttpRequestInterceptor interceptor = new AWSRequestSigningApacheInterceptor(serviceName, signer, awsStaticCredentialsProvider);
+        return new RestHighLevelClient(RestClient.builder(HttpHost.create(host)).setHttpClientConfigCallback(e -> e.addInterceptorLast(interceptor)));
     }
 }
