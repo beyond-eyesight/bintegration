df71f497ec422cdf9a5f77d44b11b072ff846156
junk-sound <wom2277@naver.com>
2021-02-21 20:06:28 +0900

    fellowship: run.sh에서 포트 기다리기 없앰

--- fellowship/run.sh
+++ fellowship/run.sh
@@ -1,16 +1,4 @@
 echo "********************************************************"
-echo "Waiting for the REDIS server to start  on port $REDIS_PORT"
-echo "********************************************************"
-while ! `nc -z redis $REDIS_PORT`; do sleep 10; done
-echo "******* REDIS has started"
-
-echo "********************************************************"
-echo "Waiting for the database server to start on port $DATABASESERVER_PORT"
-echo "********************************************************"
-while ! `nc -z cassandra $CASSANDRA_PORT`; do sleep 3; done
-echo ">>>>>>>>>>>> Database Server has started"
-
-echo "********************************************************"
 echo ">>>>>>>>>>>> Starting fellowship-service "
 echo "********************************************************"
 java -jar /usr/local/fellowship-service/fellowship-0.0.1.jar
