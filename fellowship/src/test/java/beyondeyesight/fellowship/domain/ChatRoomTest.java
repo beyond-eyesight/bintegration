c2785a875c5b8ad030b801107539569b83b5a1b7
junk-sound <wom2277@naver.com>
2021-03-21 14:48:49 +0900

    refac chatroom

--- fellowship/src/test/java/beyondeyesight/fellowship/domain/ChatRoomTest.java
+++ fellowship/src/test/java/beyondeyesight/fellowship/domain/ChatRoomTest.java
@@ -3,6 +3,7 @@ package beyondeyesight.fellowship.domain;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import beyondeyesight.fellowship.domain.model.chat.ChatRoom;
+import java.util.UUID;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -13,8 +14,9 @@ class ChatRoomTest {
     void of() {
         //given
         String name = "testRoomName";
+        UUID id = UUID.nameUUIDFromBytes(name.getBytes());
         //when
-        ChatRoom chatRoom = ChatRoom.of(name);
+        ChatRoom chatRoom = ChatRoom.of(id, name);
         //then
         assertThat(chatRoom).isNotNull();
     }
--- fellowship/src/test/java/beyondeyesight/fellowship/infra/persistence/ChatRoomCassandraRepositoryTest.java
+++ fellowship/src/test/java/beyondeyesight/fellowship/infra/persistence/ChatRoomCassandraRepositoryTest.java
@@ -3,10 +3,14 @@ package beyondeyesight.fellowship.infra.persistence;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import beyondeyesight.fellowship.config.TestCassandraConfig;
+import beyondeyesight.fellowship.domain.model.chat.ChatRoom;
+import java.util.UUID;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootContextLoader;
+import org.springframework.data.cassandra.core.CassandraOperations;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -17,7 +21,17 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 public class ChatRoomCassandraRepositoryTest {
 
     @Autowired
+    private CassandraOperations cassandraOperations;
+
+    @Autowired
     private ChatRoomCassandraRepository chatRoomCassandraRepository;
+
+    @BeforeEach
+    void setUp() {
+        String wangsimni = "Wangsimni";
+        UUID id = UUID.nameUUIDFromBytes(wangsimni.getBytes());
+        cassandraOperations.insert(ChatRoom.of(id, wangsimni));
+    }
 
     @Test
     public void findAll() {
