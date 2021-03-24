a766ecf1c10bd84a3ece77bf67b2b21c4c1e4384
junk-sound <wom2277@naver.com>
2021-03-24 14:15:20 +0900

    chore: remove trash configGlobal

--- access/src/main/java/beyondeyesight/user/config/KeycloakSecurityConfig.java
+++ access/src/main/java/beyondeyesight/user/config/KeycloakSecurityConfig.java
@@ -10,10 +10,12 @@ import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurer
 import org.keycloak.adapters.springsecurity.filter.KeycloakAuthenticationProcessingFilter;
 import org.keycloak.adapters.springsecurity.filter.QueryParamPresenceRequestMatcher;
 import org.keycloak.adapters.springsecurity.management.HttpSessionManager;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -30,17 +32,6 @@ import org.springframework.security.web.util.matcher.RequestMatcher;
 @EnableWebSecurity
 @ComponentScan(basePackageClasses = KeycloakSecurityComponents.class)
 public class KeycloakSecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
-
-//    @Autowired
-//    public void configureGlobal(
-//        AuthenticationManagerBuilder auth) throws Exception {
-//
-//        KeycloakAuthenticationProvider keycloakAuthenticationProvider
-//            = keycloakAuthenticationProvider();
-//        keycloakAuthenticationProvider.setGrantedAuthoritiesMapper(
-//            new SimpleAuthorityMapper());
-//        auth.authenticationProvider(keycloakAuthenticationProvider);
-//    }
 
     @Bean
     public KeycloakSpringBootConfigResolver KeycloakConfigResolver() {
