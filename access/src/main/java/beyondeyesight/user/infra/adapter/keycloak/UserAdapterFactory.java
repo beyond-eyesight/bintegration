68776c4f6721776eb12a66c4a973e2fa05ec48ed
junk-sound <wom2277@naver.com>
2021-03-28 10:27:31 +0900

    한번 getOperationalInfo 따라해봄. 확인은 아직 못함

--- access/src/main/java/beyondeyesight/user/infra/adapter/keycloak/UserAdapterFactory.java
+++ access/src/main/java/beyondeyesight/user/infra/adapter/keycloak/UserAdapterFactory.java
@@ -1,16 +1,20 @@
 package beyondeyesight.user.infra.adapter.keycloak;
 
 import beyondeyesight.user.infra.persistence.jpa.keycloak.UserRepository;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.keycloak.component.ComponentModel;
 import org.keycloak.models.KeycloakSession;
+import org.keycloak.provider.ServerInfoAwareProviderFactory;
 import org.keycloak.storage.UserStorageProviderFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class UserAdapterFactory implements UserStorageProviderFactory<UserAdapter>{
+public class UserAdapterFactory implements UserStorageProviderFactory<UserAdapter>,
+    ServerInfoAwareProviderFactory {
 
     @Autowired
     private UserRepository userRepository;
@@ -24,5 +28,12 @@ public class UserAdapterFactory implements UserStorageProviderFactory<UserAdapte
     @Override
     public String getId() {
         return "user-adapter";
+    }
+
+    @Override
+    public Map<String, String> getOperationalInfo() {
+        Map<String, String> ret = new LinkedHashMap<>();
+        ret.put("theme-name", "my-theme");
+        return ret;
     }
 }
